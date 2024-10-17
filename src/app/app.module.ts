9343c1297353e06e3529be122ab27d776cf4d3ed
aelixs <aelixs.dutaud@gmail.com>
2024-10-17 19:54:36 +0200

    app bar + theme management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,25 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 // Core
 import { AppRouterModule, routes } from '@core/app-router.module';
 import { LayoutModule } from '@core/layout/layout.module';
+import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRouterModule, LayoutModule],
+  imports: [
+    BrowserModule,
+    AppRouterModule,
+    LayoutModule,
+    MatSidenavContainer,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
   bootstrap: [AppComponent],
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()],
 })
