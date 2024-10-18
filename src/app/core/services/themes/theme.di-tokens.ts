94289cba84d09fb85aec0bee4a517e1b63009742
aelixs <aelixs.dutaud@gmail.com>
2024-10-18 09:44:59 +0200

    merge commit 2

--- src/app/core/services/themes/theme.di-tokens.ts
+++ src/app/core/services/themes/theme.di-tokens.ts
@@ -1,6 +1,4 @@
 import { InjectionToken } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-export const THEME = new InjectionToken<
-    (theme: 'dark' | 'light') => BehaviorSubject<string>
->('[THEME] current Theme');
+export const THEME = new InjectionToken<(theme: 'dark' | 'light') => BehaviorSubject<string>>('[THEME] current Theme');
